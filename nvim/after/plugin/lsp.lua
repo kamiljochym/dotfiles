d618c131ece68c6bbbd1879f1c1327eba3566b66
kamiljochym <kamiljochym@gmail.com>
2023-05-03 22:00:41 +0200

    changes

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -4,6 +4,7 @@ lsp.ensure_installed({
 	"tsserver",
 	"eslint",
 	"lua_ls",
+	"clangd",
 })
 
 -- Fix Undefined global 'vim'
@@ -43,7 +44,33 @@ lsp.on_attach(function(client, bufnr)
 	vim.keymap.set("n", "gd", function()
 		vim.lsp.buf.definition()
 	end, opts)
-
+	vim.keymap.set("n", "K", function()
+		vim.lsp.buf.hover()
+	end, opts)
+	vim.keymap.set("n", "<leader>vws", function()
+		vim.lsp.buf.workspace_symbol()
+	end, opts)
+	vim.keymap.set("n", "<leader>vd", function()
+		vim.diagnostic.open_float()
+	end, opts)
+	vim.keymap.set("n", "[d", function()
+		vim.diagnostic.goto_next()
+	end, opts)
+	vim.keymap.set("n", "]d", function()
+		vim.diagnostic.goto_prev()
+	end, opts)
+	vim.keymap.set("n", "<leader>vca", function()
+		vim.lsp.buf.code_action()
+	end, opts)
+	vim.keymap.set("n", "<leader>vrr", function()
+		vim.lsp.buf.references()
+	end, opts)
+	vim.keymap.set("n", "<leader>vrn", function()
+		vim.lsp.buf.rename()
+	end, opts)
+	vim.keymap.set("i", "<C-h>", function()
+		vim.lsp.buf.signature_help()
+	end, opts)
 	if client.supports_method("textDocument/formatting") then
 		vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
 		vim.api.nvim_create_autocmd("BufWritePre", {
--- nvim/lua/config/remap.lua
+++ nvim/lua/config/remap.lua
@@ -1,2 +1,2 @@
-vilua-language-serverm.g.mapleader = " "
+vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>b", vim.cmd.NvimTreeFocus)
